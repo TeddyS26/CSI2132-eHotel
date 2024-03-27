672d6c34fb09a56e564da6ad2c7011778b141ad5
TeddyS26 <tsann010@uottawa.ca>
2024-03-27 14:47:24 -0400

    chnaged trigger

--- Database Query/triggers_constraints.sql
+++ Database Query/triggers_constraints.sql
@@ -14,21 +14,23 @@ CREATE TRIGGER CheckDatesBeforeInsertOrUpdate
 BEFORE INSERT OR UPDATE ON Booking_Renting
 FOR EACH ROW EXECUTE FUNCTION check_dates_before_insert_or_update();
 
--- Trigger function for checking room-related numbers are greater than 0
-CREATE OR REPLACE FUNCTION check_room_numbers_before_insert_or_update()
+-- Trigger function for updating the room status when a customers card information is updated
+CREATE OR REPLACE FUNCTION update_room_status_after_card_update()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.room_number <= 0 THEN
-        RAISE EXCEPTION 'Room number must be greater than 0.';
+    RAISE NOTICE 'Updating room status of booking ID: %', NEW.bookingId;
+    IF (OLD.card_number <> NEW.card_number) OR (OLD.expiration_date <> NEW.expiration_date) OR (OLD.cvv <> NEW.cvv) THEN
+        RAISE NOTICE 'Updating room status to renting.';
+        NEW.status := 'Renting';
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Trigger for the function check_room_numbers_before_insert_or_update
-CREATE TRIGGER CheckRoomNumbersBeforeInsertOrUpdate
-BEFORE INSERT OR UPDATE ON Room
-FOR EACH ROW EXECUTE FUNCTION check_room_numbers_before_insert_or_update();
+-- Trigger for the function update_room_status_after_card_update
+CREATE TRIGGER UpdateRoomStatusAfterCardUpdate
+BEFORE UPDATE OF card_number, expiration_date, cvv ON Booking_Renting
+FOR EACH ROW EXECUTE FUNCTION update_room_status_after_card_update();
 
 -- Alter table to add a check constraint for SSN format for Customer
 ALTER TABLE Customer
